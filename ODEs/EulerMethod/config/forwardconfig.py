d2f9c04f827feba2f78b87fa535d448c241dcc3a
Arsha <ar.niksa@outlook.com>
2022-09-10 16:38:08 +0430

    Delete ODEs/EulerMethod directory

--- ODEs/EulerMethod/config/forwardconfig.py
+++
@@ -1,8 +0,0 @@
-# ---------------------------- importing functions --------------------------- #
-from config.solverconfig import *
-
-# ---------------------------- defining functions ---------------------------- #
-def advanceIterationByForwardMethod(xPrevious, zPrevious):
-    xUpdated = xPrevious + h * f(xPrevious, zPrevious)
-    zUpdated = zPrevious + h * s(xPrevious, zPrevious)
-    return xUpdated, zUpdated
--- ODEs/EulerMethod/config/symplecticconfig.py
+++
@@ -1,8 +0,0 @@
-# ---------------------------- importing functions --------------------------- #
-from config.solverconfig import *
-
-# ---------------------------- defining functions ---------------------------- #
-def advanceIterationBySymplecticMethod(xPrevious, zPrevious):
-    xUpdated = xPrevious + h * zPrevious
-    zUpdated = zPrevious - h * alpha * xUpdated
-    return xUpdated, zUpdated
