37da4535adb7e32d3bb6cb183eb43de261639ed9
Arsha <ar.niksa@outlook.com>
2022-09-10 17:00:28 +0430

    Delete PDEs/RelaxationMethod directory

--- PDEs/RelaxationMethod/convergence.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from config import *
-
-def checkConvergence(voltageMatrix, previousSum, newSum):
-    return abs(newSum - previousSum) < tolerance
-
-def getSumOfMatrix(voltageMatrix):
-    return np.sum(np.sum(voltageMatrix, axis = 1), axis = 0)
--- PDEs/RelaxationMethod/solver.py
+++
@@ -1,8 +0,0 @@
-from config import *
-
-def computeWeightedAverage(voltageMatrix, i, j):
-    computedAverage = 1 / (2 * (hx*hx + hy*hy)) * \
-        (hx*hx * (voltageMatrix[i, j+1] + voltageMatrix[i, j-1]) + \
-            hy*hy * (voltageMatrix[i+1, j] + voltageMatrix[i-1, j]))
-
-    return computedAverage
