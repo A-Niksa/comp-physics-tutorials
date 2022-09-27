ab54b9032f6e00295750af04e6381a8cbe0565ea
Arsha <ar.niksa@outlook.com>
2022-09-28 00:07:56 +0330

    Delete derivative_calculator.py

--- root_finding/derivative_calculator.py
+++
@@ -1,9 +0,0 @@
-# importing config
-from config import derivative_increment
-
-
-# defining the derivative calculation function
-def calculate_derivative(x, function):
-    value_at_back = function(x - derivative_increment)
-    value_at_front = function(x + derivative_increment)
-    return (value_at_front - value_at_back) / (2 * derivative_increment)
