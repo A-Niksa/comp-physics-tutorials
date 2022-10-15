2f6894a52767970a3b31e18e5ae8b735e8dcaf67
Arsha Niksa <ar.niksa@outlook.com>
2022-10-16 00:39:08 +0330

    Delete PDEs/exercises/heating_plate_with_hole directory

--- PDEs/exercises/heating_plate_with_hole/solution/node_averaging_calculator.py
+++
@@ -1,3 +0,0 @@
-# defining the average calculator function:
-def calculate_neighboring_nodes_average(nodes_matrix, i, j):
-    return (nodes_matrix[i, j-1] + nodes_matrix[i, j+1] + nodes_matrix[i-1, j] + nodes_matrix[i+1, j]) / 4
